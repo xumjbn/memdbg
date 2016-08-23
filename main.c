308397cbc28c0f5247136ca2a9c30d002c25ed33
xumjbn <xumjns@hotmail.com>
2016-08-23 10:14:41 -0400

    fix

--- main.c
+++ main.c
@@ -7,25 +7,28 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
-#include "dbgmem.h"
+#include "memdbg.h"
 
 #define MALLOC(size)		dbg_malloc(size, __FILE__, __LINE__)
-#define FREE(ptr)			dbg_free(ptr)
+#define FREE(ptr)		dbg_free(ptr)
+
+#define malloc	MALLOC
+#define free	FREE
 
 int main(void) 
 {
 	int *a;
 	
 	for (int i = 0; i < 1; i++) {
-		a = MALLOC(sizeof(int));
+		a = malloc(sizeof(int));
 		if (a) {
-			FREE(a);
+			free(a);
 		}
 	}
-	int *b = MALLOC(123);
-	FREE(b);
+	int *b = malloc(123);
+	free(b);
 
-	MALLOC(12345);
+	malloc(12345);
 
 	print_meminfo();
 
